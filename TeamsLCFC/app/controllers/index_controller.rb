43adfb91ea77eb5753e77e0b5e0583076953fdaf
Matías <matiasdim3@gmail.com>
2015-11-24 20:13:25 -0500

    Setting up views

--- TeamsLCFC/app/controllers/index_controller.rb
+++ TeamsLCFC/app/controllers/index_controller.rb
@@ -1,4 +1,11 @@
 class IndexController < ApplicationController
   def index
   end
+
+  def mix_teams
+  
+  end
+  def about
+
+  end
 end
--- TeamsLCFC/config/routes.rb
+++ TeamsLCFC/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'index/index'
+  get 'index/mix_teams'
+  get 'index/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
